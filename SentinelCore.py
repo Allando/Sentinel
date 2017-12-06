d41f9f3116e80d6d754e0287d79f6c49fd10c980
Allando <alla2634@edu.easj.dk>
2017-12-06 16:21:06 +0100

    added to sentinelcore and sentinel

---
+++ SentinelCore.py
@@ -0,0 +1,22 @@
+"""
+This is the Convolutional Neural Network module of Sentinel
+
+Concept: 3
+"""
+
+import numpy as np
+
+class Sentinel_Core:
+
+    def __init__(self, sizes):
+        self.sizes = sizes
+        self.num_layers = len(sizes)
+        self.weights = [np.random.randn(y, 1) for y in sizes[1:]]
+        sefl.biases = [np.random.randn(y, x)
+                for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def sigmoid(self, a, w, b):
+        return 1/(1+np.exp(-np.sum(w, a, -b)))
+
+    def cost(self, w, b):
+        
--- sentinel.py
+++ sentinel.py
@@ -1,6 +1,50 @@
-#!/bin/python env
+#!/bin/python  
 
-import numpy as np
+import SentinelCore
 
-print("Hello")
+
+def main():
+    title()
+    
+    print("1) Attack Mode\n2) Training Mode\n3) Exit")
+    
+
+    choice = int(input("What would you like to do?: "))
+    
+    if choice == 1: attack_mode()
+    elif choice == 2: training_mode()
+    elif choice == 3:
+        print ("Exiting...")
+        exit(0)
+    else:
+        print("Wrong command")
+
+
+def attack_mode():
+    print("attack module is not implementet yet")
+    main()
+
+
+def training_mode():
+    print("training module is not implementet yet")
+    main()
+
+
+"""
+This is just the title when the program is run.
+It spells out a big ol' "SENTINEL"
+"""
+def title():
+    titleList = [" _______  _______  __    _  _______  ___  __    _  _______  ___     ",
+    "|       ||       ||  |  | ||       ||   ||  |  | ||       ||   |    ",
+    "|  _____||    ___||   |_| ||_     _||   ||   |_| ||    ___||   |    ",
+    "| |_____ |   |___ |       |  |   |  |   ||       ||   |___ |   |    ",
+    "|_____  ||    ___||  _    |  |   |  |   ||  _    ||    ___||   |___ ",
+    " _____| ||   |___ | | |   |  |   |  |   || | |   ||   |___ |       |",
+    "|_______||_______||_|  |__|  |___|  |___||_|  |__||_______||_______|"]
+
+    for l in titleList:
+        print(l)
+    print("")
+main()
 
