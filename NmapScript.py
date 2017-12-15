a293ea6cb4572bc0dc38d6adbb468e55146da108
root <alla2634@edu.easj.dk>
2017-12-15 04:17:19 +0100

    created my own nmap program

--- NmapScript.py
+++ NmapScript.py
@@ -10,7 +10,29 @@ import os
 # Third party libraries
 import numpy as np
 
-ip_address = "192.168.0.108"
-ports = np.array([21, 22, 23, 80, 445])
 
-print(os.system("sudo nmap {} -O".format(ip_address)))
+class Nmap:
+    def __init__(self, ip_range, port_range=None, commands=None):
+        self.ip_range = ip_range
+        self.port_range = port_range
+        self.commands = commands
+
+    def scan_network(self):
+        if self.port_range is not None:
+            return os.system("sudo nmap {} -p {} {}".format(self.ip_range, self.port_range, self.commands))
+        else:
+            return os.system("sudo nmap {} {}".format(self.ip_range, self.commands))
+
+
+nm0 = Nmap("127.0.0.1")
+nm1 = Nmap("127.0.0.1", "21-23", "-sn")
+nm2 = Nmap("127.0.0.1", "21-23")
+nm3 = Nmap("127.0.0.1", "21", "-sn")
+nm4 = Nmap("127.0.0.1", "21")
+
+print("0: ", nm0.scan_network())
+print("1: ", nm1.scan_network())
+print("2: ", nm2.scan_network())
+print("3: ", nm3.scan_network())
+print("4: ", nm4.scan_network())
+
