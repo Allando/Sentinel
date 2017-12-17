9424b597d8dda0d5fc86116bc747cff1c352cf0a
Unknown <alla2634@edu.easj.dk>
2017-12-18 00:56:52 +0100

    Rewrote nmap added temp directory

--- NmapScript.py
+++ NmapScript.py
@@ -5,24 +5,40 @@ This is a script which uses nmap through shell command.'
 """
 
 # Standard libraries
-import os
+import subprocess
 
 # Third party libraries
 import numpy as np
-import nmap
 
-nm = nmap.PortScanner() # instantiate nmap.PortScanner object
-nm.scan('192.168.0.0/24')
 
-for host in nm.all_hosts():
-    print('----------------------------------------------------')
-    print('Host : %s (%s)' % (host, nm[host].hostname()))
-    print('State : %s' % nm[host].state())
-    for proto in nm[host].all_protocols():
-        print('----------')
-        print('Protocol : %s' % proto)
+class Nmap:
+    def __init__(self, ip_range):
+        self.ip_range = ip_range
+        self.filepath = "temp/nmap_output.xml"
 
-        lport = nm[host][proto].keys()
-        for port in lport:
-            if nm[host][proto][port]['state'] == 'open':
-                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    def scan(self, port_range=None, arguments=None):
+        command_line = "nmap {} -oX -".format(self.ip_range)
+
+        if port_range == None and arguments == None:
+            command_line = command_line
+        elif port_range != None and arguments == None:
+            command_line = command_line + " -p {}".format(port_range)
+        elif port_range == None and arguments != None:
+            command_line = command_line + " " + arguments
+        elif port_range != None and arguments != None:
+            command_line = command_line + " -p {} {}".format(port_range, arguments)
+        else:
+            print("Check ya input again...")
+
+        f = open(self.filepath, "w")
+        subprocess.run(command_line, stdout=f, shell=True)
+        f.close()
+
+    def print_output(self):
+        pass
+
+
+IppoNmap = Nmap("192.168.0.108")
+scanning = IppoNmap.scan("21-24", "-Pn")
+
+print(IppoNmap.print_output())
