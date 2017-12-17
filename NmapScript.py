d126aaa1711029fd6d52cb354ae5f57534830b9f
Allando <alla2634@edu.easj.dk>
2017-12-17 13:20:55 +0100

    added nmap

--- NmapScript.py
+++ NmapScript.py
@@ -9,31 +9,20 @@ import os
 
 # Third party libraries
 import numpy as np
+import nmap
 
+nm = nmap.PortScanner() # instantiate nmap.PortScanner object
+nm.scan('192.168.0.0/24')
 
-class Nmap:
-    def __init__(self, ip_range, port_range=None, commands=None):
-        self.ip_range = ip_range
-        self.port_range = port_range
-        self.commands = commands
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('----------')
+        print('Protocol : %s' % proto)
 
-    def scan_network(self):
-        if self.port_range is not None:
-            return os.system("sudo nmap {} -p {} {}".format(self.ip_range, self.port_range, self.commands))
-        else:
-            return os.system("sudo nmap {} {}".format(self.ip_range, self.commands))
-
-
-hosts = "10.200.126.0/24"
-nm0 = Nmap(hosts)
-nm1 = Nmap(hosts, "21-23", "-sn")
-nm2 = Nmap(hosts, "21-23")
-nm3 = Nmap(hosts, "21", "-sn")
-nm4 = Nmap(hosts, "21")
-
-print("0: ", nm0.scan_network())
-print("1: ", nm1.scan_network())
-print("2: ", nm2.scan_network())
-print("3: ", nm3.scan_network())
-print("4: ", nm4.scan_network())
-
+        lport = nm[host][proto].keys()
+        for port in lport:
+            if nm[host][proto][port]['state'] == 'open':
+                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
