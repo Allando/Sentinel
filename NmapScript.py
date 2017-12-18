e23f936054232f56542d7db3c5575e344a6bbb34
root <alla2634@edu.easj.dk>
2017-12-18 14:25:09 +0100

    Wrote to nmapscript... shits still not working

--- NmapScript.py
+++ NmapScript.py
@@ -5,27 +5,28 @@ This is a script which uses nmap through shell command.'
 """
 
 # Standard libraries
+import os
 import subprocess
+import xml.etree.ElementTree as ET
 
 # Third party libraries
-import numpy as np
 
 
 class Nmap:
     def __init__(self, ip_range):
         self.ip_range = ip_range
-        self.filepath = "temp/nmap_output.xml"
+        self.filepath = os.path.abspath("temp/nmap_output.xml")
 
     def scan(self, port_range=None, arguments=None):
         command_line = "nmap {} -oX -".format(self.ip_range)
 
-        if port_range == None and arguments == None:
+        if port_range is None and arguments is None:
             command_line = command_line
-        elif port_range != None and arguments == None:
+        elif port_range is not None and arguments is None:
             command_line = command_line + " -p {}".format(port_range)
-        elif port_range == None and arguments != None:
+        elif port_range is None and arguments is not None:
             command_line = command_line + " " + arguments
-        elif port_range != None and arguments != None:
+        elif port_range is not None and arguments is not None:
             command_line = command_line + " -p {} {}".format(port_range, arguments)
         else:
             print("Check ya input again...")
@@ -35,10 +36,18 @@ class Nmap:
         f.close()
 
     def print_output(self):
-        pass
+        tree = ET.parse(self.filepath)
+        root = tree.getroot()
+
+        for child in root.findall('host'):
+            port = child.find('ports').text
+            port2 = child.get('ports')
+
+            print(port, port2)
+
+        # ports = dom.findall('nmaprun/host/ports/port/state')
 
 
-IppoNmap = Nmap("192.168.0.108")
-scanning = IppoNmap.scan("21-24", "-Pn")
-
-print(IppoNmap.print_output())
+ippo_nmap = Nmap("")
+ippo_nmap.scan()
+ippo_nmap.print_output()
